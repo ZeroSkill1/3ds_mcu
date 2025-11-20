16604413061cae8145ece6ffec01250683d0b3b1
ZeroSkill1 <timmskiller102@outlook.com>
2025-11-20 18:21:22 +0300

    use more efficient BCD <-> int implementation

--- include/util.h
+++ include/util.h
@@ -1,8 +1,8 @@
 #ifndef MCU_UTIL_H
 #define MCU_UTIL_H
 
-#define INT2BCD(x) ((((x) / 10) << 4) | ((x) % 10))
-#define BCD2INT(x) ((((x) >> 4) * 10) | ((x) & 0xF))
+#define INT2BCD(x) ((x) + 6u * (((x) * 103u)>>10))
+#define BCD2INT(x) ((x) - 6u * ((x)>>4))
 #define CHECKBIT(x,y) (((x) & (y)) == (y))
 
 #define LODWORD(x) ((u32)(x & 0xFFFFFFFF))
